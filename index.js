811946a2176d785ddabf0cbb3cd1e7e4549aab33
Naimur Rahib <61423411+7rahib@users.noreply.github.com>
2022-05-19 03:03:04 +0600

    jwt and user api created

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const cors = require('cors');
@@ -12,13 +13,41 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'UnAuthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
 
 async function run() {
     try {
         await client.connect();
         const servicesCollection = client.db('doctors_portal').collection('services');
         const bookingCollection = client.db('doctors_portal').collection('bookings');
+        const userCollection = client.db('doctors_portal').collection('users');
+
+
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
+            res.send({ result, token });
+        })
 
         app.get('/service', async (req, res) => {
             const query = {};
@@ -27,11 +56,22 @@ async function run() {
             res.send(services);
         })
 
-        app.get('/booking', async (req, res) => {
+        app.get('/user', verifyJWT, async (req, res) => {
+            const users = await userCollection.find().toArray();
+            res.send(users);
+        });
+
+        app.get('/booking', verifyJWT, async (req, res) => {
             const patient = req.query.patient;
-            const query = { patient: patient };
-            const bookings = await bookingCollection.find(query).toArray();
-            res.send(bookings);
+            const decodedEmail = req.decoded.email;
+            if (patient === decodedEmail) {
+                const query = { patient: patient };
+                const bookings = await bookingCollection.find(query).toArray();
+                return res.send(bookings);
+            }
+            else {
+                return res.status(403).send({ message: 'forbidden access' });
+            }
         })
 
         app.post('/booking', async (req, res) => {
@@ -48,34 +88,20 @@ async function run() {
         app.get('/available', async (req, res) => {
             const date = req.query.date;
 
-            // step 1:  get all services
             const services = await servicesCollection.find().toArray();
-
-            // step 2: get the booking of that day. output: [{}, {}, {}, {}, {}, {}]
             const query = { date: date };
             const bookings = await bookingCollection.find(query).toArray();
 
-            // step 3: for each service
             services.forEach(service => {
-                // step 4: find bookings for that service. output: [{}, {}, {}, {}]
                 const serviceBookings = bookings.filter(book => book.treatment === service.name);
-                // step 5: select slots for the service Bookings: ['', '', '', '']
                 const bookedSlots = serviceBookings.map(book => book.slot);
-                // step 6: select those slots that are not in bookedSlots
                 const available = service.slots.filter(slot => !bookedSlots.includes(slot));
-                //step 7: set available to slots to make it easier 
                 service.slots = available;
             });
-
-
             res.send(services);
         })
-
-
     }
-    finally {
-
-    }
+    finally { }
 }
 
 run().catch(console.dir)
